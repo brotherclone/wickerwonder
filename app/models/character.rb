304e4e454cdf4740d3c315163fdc707535f7cbe1
Gabriel Walsh <gawalsh@adobe.com>
2015-01-30 16:19:51 -0500

    missing imageable from factory

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ActiveRecord::Base
-  has_one :image, as: :imageable
+  has_one :img, :as => :imageable
+  #Todo put dependent destory once imageable is in factory
   validates :name, presence: true
 end
