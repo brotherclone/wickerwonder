4a67382b855ecf129d35fca13e740a046afac1f6
Gabriel Walsh <gawalsh@adobe.com>
2015-01-30 09:28:55 -0500

    navigation spec complete

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 include NavHelper
 
 
+describe 'Main Navigation is Available' do
+
+  before (:each) do
+    visit root_path
+  end
+
+  it 'checks for main navigation presence' do
+    expect(page).to have_selector('div#main_navigation')
+  end
+
+  it 'checks that links are there and correct' do
+    navnames = NavHelper.short.values
+    navnames.each do |n|
+      expect(page).to have_selector('a.item', :text => n)
+    end
+  end
+
+end
 
 describe 'Side Navigation is Available' do
 
@@ -26,26 +44,11 @@ describe 'Side Navigation is Available' do
     expect(page).not_to have_selector('div.dimmed')
   end
 
-  it 'checks for main navigation presence' do
-    expect(page).to have_selector('div')
-  end
-
   it 'checks that links are there and correct' do
-    navnames = NavHelper.short.values
+    navnames = NavHelper.long.values
     navnames.each do |n|
       expect(page).to have_selector('a.item', :text => n)
     end
-  end
-
-=begin
-  it 'checks that the home button is available in the side navigation', :js => true do
-    click_button('Menu')
-    expect(page).to have_selector('a.home', :text => 'Home')
-  end
-
-  it 'checks for the character menu header', :js =>true do
-    click_button('Menu')
-    expect(page).to have_selector('h4.header', :text => 'Meet the characters')
   end
 
   it 'makes sure there are less than 20 Characters in the nav', :js => true do
@@ -53,15 +56,5 @@ describe 'Side Navigation is Available' do
     expect(page).to have_selector('a.shoppecharacter', :between => 1..20)
   end
 
-  it 'checks for a comic button', :js => true do
-    click_button('Menu')
-    expect(page).to have_selector('a', :text => 'Get the comic')
-  end
-
-  it 'checks for a pitch materials', :js => true do
-    click_button('Menu')
-    expect(page).to have_selector('a', :text => 'Pitch materials')
-  end
-=end
 
 end
