2e34748eea1f7d42a4278ad9974b69454b8f620b
Gabriel Walsh <gawalsh@adobe.com>
2015-05-22 07:51:34 -0400

    manually adding prod assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Wickerwonder
   class Application < Rails::Application
     config.serve_static_files = true
+    config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
