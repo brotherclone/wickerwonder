54007d7d17f956f4d55c3daa7283ed57f392d8b7
Gabriel Walsh <gawalsh@adobe.com>
2015-06-09 13:39:47 -0400

    situations, buttons, etc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528203422) do
+ActiveRecord::Schema.define(version: 20150609171816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150528203422) do
     t.string   "situationimg"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "headingcolor"
   end
 
 end
